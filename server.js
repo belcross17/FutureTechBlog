46fdcd351ae58c8852f12e3cdcd127ecb7fd370b
Brayan <alfredochabes1@hotmail.com>
2022-10-11 17:46:21 -0400

    created handlebars

--- server.js
+++ server.js
@@ -1,36 +1,36 @@
-const path = require('path');
-const express = require('express');
-const sequelize = require('./config/connection')
-const helpers = require('./utils/helpers')
+const path = require("path");
+const express = require("express");
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const exphbs = require('express-Handlebars');
+const exphbs = require("express-Handlebars");
 const hbs = exphbs.create({ helpers });
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
-const session = require('express-session');
+const session = require("express-session");
 
-const sequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {
-        maxAge: 2000000
-    },
-    rolling: true,
-    resave: true,
-    saveUninitialized: true, 
-    store: new sequelizeStore({
-        db: sequelize
-    }),
+  secret: "Super secret secret",
+  cookie: {
+    maxAge: 2000000,
+  },
+  rolling: true,
+  resave: true,
+  saveUninitialized: true,
+  store: new sequelizeStore({
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
@@ -38,5 +38,5 @@ app.use(session(sess));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Now Listening on ${PORT}!!`))
+  app.listen(PORT, () => console.log(`Now Listening on ${PORT}!!`));
 });
