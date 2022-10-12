6f29c39ca31f263467d40a5225b48e41308a9d9f
Brayan <alfredochabes1@hotmail.com>
2022-10-11 21:16:45 -0400

    fixed issue missing routes require in server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const express = require("express");
+const routes = require('./controllers')
 const sequelize = require("./config/connection");
 const helpers = require("./utils/helpers");
 
